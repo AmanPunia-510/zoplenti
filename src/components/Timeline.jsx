8c6bb985a498f63c51497a0aa9c0bc9835195e48
AmanPunia-510 <puniaaman888@gmail.com>
2024-09-30 15:20:04 +0530

    work on responsive ness of page

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,29 +1,35 @@
 import { TIMELINE_DATA } from '@/utils/Helper'
 import React from 'react'
+import Icon from './common/Icon'
 
 const Timeline = () => {
   return (
     <div className='relative'>
-      <div className="container !max-w-[1180px]">
+      <div className="container xl:!max-w-[1180px]">
         <div className="flex max-lg:flex-wrap lg:gap-6">
-          <div className='grid grid-rows-5 w-full gap-10 pl-7 md:pl-44 lg:pl-0 max-md:relative after:absolute after:bg-verticleBorder after:p-px after:h-full after:left-0 xl:after:left-1/2 after:top-1/2 after:-translate-y-1/2'>
+          <div className='relative grid grid-rows-5 w-full gap-10 pl-7 md:pl-44 lg:pl-0 max-md:relative after:absolute after:bg-verticleBorder after:p-px after:h-[92%] xl:after:h-[1907px] md:after:left-[57px] xl:after:left-1/2 after:top-8 pt-[86px] max-xl:justify-end'>
             {TIMELINE_DATA.map((obj, index) => (
-              <div className={`w-full max-w-[530px] p-[1px] rounded-[10px] relative max-xl:mx-auto ${index % 2 === 0 ? "m-0" : "xl:ml-auto"}`}>
-                <span className={`${index % 2 === 0 ? "max-xl:-left-40 xl:-right-40" : "md:-left-40"} md:absolute top-0 flex`}>
-                  <div className='rounded-full p-px'>
-                    <div className='size-20 bg-iconGradient rounded-full flex justify-center items-center'>
-                      {obj.icon}
-                    </div>
-                  </div>
-                </span>
-                <div className='bg-timelineGradient p-px rounded-[10px]'>
+              <div className={`w-full xl:max-w-[530px] p-[1px] rounded-[10px] relative max-xl:mx-auto ${index % 2 !== 0 ? "m-0" : "xl:ml-auto"}`}>
+                <div className='bg-timelineGradient p-px rounded-[10px] relative z-40'>
                   <div className='p-4 rounded-[10px] !bg-timelineBg'>
-                    <div key={index} className='max-w-[530px]'>
+                    <div key={index} className='max-w-[770px] xl:max-w-[530px]'>
                       <h2 className='font-bold leading-130 text-custom-lg text-white'>{obj.Heading}</h2>
-                      <p className='leading-170 text-lg text-white font-normal pt-[18px]'>{obj.Paragraph}</p>
+                      <p className='leading-170 text-lg text-white font-light pt-[18px]'>{obj.Paragraph}</p>
                     </div>
                   </div>
                 </div>
+                <span className={`${index % 2 !== 0 ? "max-xl:-left-40 xl:-right-40" : "md:-left-40"} md:absolute top-0 flex z-10 max-md:pt-6`}>
+                  <div className='rounded-full bg-borderGradient p-px'>
+                    <div className='bg-bg-color rounded-full'>
+                      <div className='size-20 bg-iconGradient rounded-full flex justify-center items-center'>
+                        {obj.icon}
+                      </div>
+                    </div>
+                    <div className={`absolute top-5 z-20 ${index % 2 !== 0 ? "xl:rotate-180 xl:-left-20 md:left-[110px] -left-9" : "-left-9 md:left-[110px]"}`}>
+                    {obj.arrow}
+                    </div>
+                  </div>
+                </span>
               </div>
             ))}
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,13 +20,14 @@ module.exports = {
         "blue": "#33B6FF",
         "grey": "#FFFFFF1A",
         "rich-white": "#FCFCFC;",
+        "bg-color" : "#000c24",
       },
       backgroundImage: {
         timelineGradient: "linear-gradient(180deg, #33B6FF -1.9%, rgba(51, 182, 255, 0.52) 58.18%, rgba(11, 6, 42, 0) 100%)",
         iconGradient: "linear-gradient(180deg, rgba(255, 255, 255, 0.1) 0%, rgba(255, 255, 255, 0) 100%)",
         borderGradient: "linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0.1) 100%)",
         hero: "url(../../public/assets/images/png/hero.png)",
-        verticleBorder: "linear-gradient(90deg, #000C24 0%, #33B6FF 10%, #33B6FF 90%, #000C24 100%)",
+        verticleBorder: "linear-gradient(101deg, #000C24 0%, #33B6FF 10%, #33B6FF 90%, #000C24 100%)",
       },
       fontSize: {
         "custom-lg": "28px",
