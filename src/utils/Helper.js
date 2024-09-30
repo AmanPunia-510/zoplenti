a54d1ed97f2b42551dbc728ad7af29979b444bb4
AmanPunia-510 <puniaaman888@gmail.com>
2024-09-30 13:54:01 +0530

    change in hero section

--- src/utils/Helper.js
+++ src/utils/Helper.js
@@ -1,22 +1,28 @@
+import Icon from "@/components/common/Icon"
 export const TIMELINE_DATA = [
     {
         Heading: "Data Tracking",
         Paragraph: "ZVP tracks 253 key data points about every SKU we sell. We track not only sales data, but crucial marketing statistics, including traffic and conversion rate. This comprehensive data collection informs our decision-making processes, from stock replenishment to targeted marketing initiatives.We then use all this data to drive our decision making, from stock replenishment to marketing initiatives.",
+        icon: <Icon icons='dataTracking' />,
     },
     {
         Heading: "Data Tracking",
         Paragraph: "ZVP tracks 253 key data points about every SKU we sell. We track not only sales data, but crucial marketing statistics, including traffic and conversion rate. This comprehensive data collection informs our decision-making processes, from stock replenishment to targeted marketing initiatives.We then use all this data to drive our decision making, from stock replenishment to marketing initiatives.",
+        icon: <Icon icons='dataTracking' />,
     },
     {
         Heading: "Data Tracking",
         Paragraph: "ZVP tracks 253 key data points about every SKU we sell. We track not only sales data, but crucial marketing statistics, including traffic and conversion rate. This comprehensive data collection informs our decision-making processes, from stock replenishment to targeted marketing initiatives.We then use all this data to drive our decision making, from stock replenishment to marketing initiatives.",
+        icon: <Icon icons='dataTracking' />,
     },
     {
         Heading: "Data Tracking",
         Paragraph: "ZVP tracks 253 key data points about every SKU we sell. We track not only sales data, but crucial marketing statistics, including traffic and conversion rate. This comprehensive data collection informs our decision-making processes, from stock replenishment to targeted marketing initiatives.We then use all this data to drive our decision making, from stock replenishment to marketing initiatives.",
+        icon: <Icon icons='dataTracking' />,
     },
     {
         Heading: "Data Tracking",
         Paragraph: "ZVP tracks 253 key data points about every SKU we sell. We track not only sales data, but crucial marketing statistics, including traffic and conversion rate. This comprehensive data collection informs our decision-making processes, from stock replenishment to targeted marketing initiatives.We then use all this data to drive our decision making, from stock replenishment to marketing initiatives.",
+        icon: <Icon icons='dataTracking' />,
     },
 ]
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,15 +13,20 @@ module.exports = {
         143: "143%",
         170: "170%",
       },
+      backgroundColor: {
+        "timelineBg": "#040A26",
+      },
       colors: {
         "blue": "#33B6FF",
         "grey": "#FFFFFF1A",
-        "rich-white": "#FCFCFC;"
+        "rich-white": "#FCFCFC;",
       },
       backgroundImage: {
         timelineGradient: "linear-gradient(180deg, #33B6FF -1.9%, rgba(51, 182, 255, 0.52) 58.18%, rgba(11, 6, 42, 0) 100%)",
         iconGradient: "linear-gradient(180deg, rgba(255, 255, 255, 0.1) 0%, rgba(255, 255, 255, 0) 100%)",
         borderGradient: "linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0.1) 100%)",
+        hero: "url(../../public/assets/images/png/hero.png)",
+        verticleBorder: "linear-gradient(90deg, #000C24 0%, #33B6FF 10%, #33B6FF 90%, #000C24 100%)",
       },
       fontSize: {
         "custom-lg": "28px",
@@ -34,9 +39,6 @@ module.exports = {
           DEFAULT: "20px",
         },
       },
-      backgroundImage: {
-        hero: "url(../../public/assets/images/png/hero.png)",
-      }
     },
   },
   plugins: [],
