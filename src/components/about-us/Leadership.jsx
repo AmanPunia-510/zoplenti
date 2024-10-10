7d5299c249303db6d4a6a60f3bd59a70ecb1b2f5
Ritik <rvjaat25@gmail.com>
2024-10-10 10:14:36 +0530

    page done

--- src/components/about-us/Leadership.jsx
+++ src/components/about-us/Leadership.jsx
@@ -4,13 +4,13 @@ const Leadership = () => {
   return (
     <div className='py-20 sm:py-24 xl:py-[100px]'>
       <div className="container max-w-[1180px]">
-        <div className="border border-grey w-full"></div>
-        <h2 className="font-bold text-5xl leading-133">Meet The Leadership Team</h2>
+        <div className="border-[2px] border-picton-blue opacity-20 w-full"></div>
+        <h2 className="font-bold text-5xl leading-133 text-white text-center mt-[100px]">Meet The Leadership Team</h2>
         <Image
-          className='rounded-3xl border border-picton-blue my-20 sm:my-24 xl:my-[140px] h-[190px] sm:h-auto'
-          src='/assets/images/webp/fintonist.webp'
-          width={1440}
-          height={480}
+          className='mb-20 sm:mb-24 xl:mb-[140px] h-[190px] sm:h-auto mt-[60px]'
+          src='/assets/images/about-us/webp/meet-the-team.webp'
+          width={1140}
+          height={497}
           alt='Fitonist'
         />
         <div className="border border-grey w-full"></div>
